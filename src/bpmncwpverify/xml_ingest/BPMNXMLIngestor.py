f3e0ffc6eda92375e309491a414e196099d274e5
Ty Klabacka <klabacka.ty@gmail.com>
2024-10-17 04:20:36 +0000

    added defusedxml as a dependency

--- src/bpmncwpverify/xml_ingest/BPMNXMLIngestor.py
+++ src/bpmncwpverify/xml_ingest/BPMNXMLIngestor.py
@@ -1,7 +1,13 @@
 from ..bpmn.BPMN import Bpmn
+from defusedxml.ElementTree import parse
 
 
 class BPMNXMLIngestor:
     @staticmethod
-    def get_bpmn_from_xml(xml: str) -> Bpmn:
+    def get_bpmn_from_xml(xml_file: str) -> Bpmn:
+        tree = parse(xml_file)
+
+        root = tree.getroot()
+        print(root)
+
         return Bpmn()
