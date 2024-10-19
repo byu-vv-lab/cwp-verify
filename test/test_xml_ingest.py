6a2002217d7b941c6d406d6a09d550017dd1a79d
Ty Klabacka <klabacka.ty@gmail.com>
2024-10-19 05:38:20 +0000

    fixed test_xml_ingest to go with new changes

--- test/test_xml_ingest.py
+++ test/test_xml_ingest.py
@@ -31,7 +31,7 @@ def assert_flow(process, flow_id, source_id, target_id):
     assert flow.target_node.id == target_id, f"Flow {flow_id} target node mismatch"
 
 
-def test_get_root():
+def test_xml_parser():
     current_directory = os.path.dirname(os.path.abspath(__file__))
 
     workflow_bpmn_path = os.path.join(current_directory, "example", "workflow.bpmn")
@@ -45,7 +45,7 @@ def test_get_root():
     assert process.id == "Process_1xbpt9j"
 
     # Gateways
-    gateway_12r266n = process.elements.get("Gateway_12r266n")
+    gateway_12r266n = process["Gateway_12r266n"]
     assert isinstance(gateway_12r266n, ParallelGatewayNode)
     assert gateway_12r266n is not None
     assert gateway_12r266n.name == "both"
@@ -56,7 +56,7 @@ def test_get_root():
     assert "Flow_1kx5xjv" in {flow.id for flow in gateway_12r266n.out_flows}
     assert "Flow_13xpfx7" in {flow.id for flow in gateway_12r266n.out_flows}
 
-    gateway_0s1i42o = process.elements.get("Gateway_0s1i42o")
+    gateway_0s1i42o = process["Gateway_0s1i42o"]
     assert gateway_0s1i42o is not None
     assert gateway_0s1i42o.name == "end both"
     assert len(gateway_0s1i42o.in_flows) == 2
@@ -65,7 +65,7 @@ def test_get_root():
     assert "Flow_1oezfcg" in {flow.id for flow in gateway_0s1i42o.in_flows}
     assert "Flow_0feadgd" in {flow.id for flow in gateway_0s1i42o.out_flows}
 
-    gateway_1pm4ghz = process.elements.get("Gateway_1pm4ghz")
+    gateway_1pm4ghz = process["Gateway_1pm4ghz"]
     assert gateway_1pm4ghz is not None
     assert gateway_1pm4ghz.name == "payment and terms agreed"
     assert len(gateway_1pm4ghz.in_flows) == 3
@@ -79,7 +79,7 @@ def test_get_root():
     assert "Flow_0koz64j" in {flow.id for flow in gateway_1pm4ghz.out_flows}
     assert "Flow_08e7qxg" in {flow.id for flow in gateway_1pm4ghz.out_flows}
 
-    gateway_000lymc = process.elements.get("Gateway_000lymc")
+    gateway_000lymc = process["Gateway_000lymc"]
     assert gateway_000lymc is not None
     assert gateway_000lymc.name == "both1"
     assert len(gateway_000lymc.in_flows) == 1
@@ -88,7 +88,7 @@ def test_get_root():
     assert "Flow_0jmvvxc" in {flow.id for flow in gateway_000lymc.out_flows}
     assert "Flow_1y66pph" in {flow.id for flow in gateway_000lymc.out_flows}
 
-    gateway_0cy7rs7 = process.elements.get("Gateway_0cy7rs7")
+    gateway_0cy7rs7 = process["Gateway_0cy7rs7"]
     assert gateway_0cy7rs7 is not None
     assert gateway_0cy7rs7.name == "end both1"
     assert len(gateway_0cy7rs7.in_flows) == 2
@@ -98,7 +98,7 @@ def test_get_root():
     assert "Flow_1cm84v3" in {flow.id for flow in gateway_0cy7rs7.out_flows}
 
     # Activities
-    activity_1unsjkg = process.elements.get("Activity_1unsjkg")
+    activity_1unsjkg = process["Activity_1unsjkg"]
     assert activity_1unsjkg is not None
     assert activity_1unsjkg.name == "2-Buyer and Seller negotiate terms"
     assert len(activity_1unsjkg.in_flows) == 1
@@ -106,7 +106,7 @@ def test_get_root():
     assert "Flow_1kx5xjv" in {flow.id for flow in activity_1unsjkg.in_flows}
     assert "Flow_1oezfcg" in {flow.id for flow in activity_1unsjkg.out_flows}
 
-    activity_1t579ox = process.elements.get("Activity_1t579ox")
+    activity_1t579ox = process["Activity_1t579ox"]
     assert activity_1t579ox is not None
     assert activity_1t579ox.name == "3-Buyer and Seller negotiate price"
     assert len(activity_1t579ox.in_flows) == 1
@@ -114,7 +114,7 @@ def test_get_root():
     assert "Flow_13xpfx7" in {flow.id for flow in activity_1t579ox.in_flows}
     assert "Flow_14s5onf" in {flow.id for flow in activity_1t579ox.out_flows}
 
-    activity_1qm7qck = process.elements.get("Activity_1qm7qck")
+    activity_1qm7qck = process["Activity_1qm7qck"]
     assert activity_1qm7qck is not None
     assert activity_1qm7qck.name == "1-Buyer sees desired backpack and meets Seller"
     assert len(activity_1qm7qck.in_flows) == 1
@@ -122,7 +122,7 @@ def test_get_root():
     assert "Flow_0oiwgjd" in {flow.id for flow in activity_1qm7qck.in_flows}
     assert "Flow_1wl740o" in {flow.id for flow in activity_1qm7qck.out_flows}
 
-    activity_1qqx4aq = process.elements.get("Activity_1qqx4aq")
+    activity_1qqx4aq = process["Activity_1qqx4aq"]
     assert activity_1qqx4aq is not None
     assert activity_1qqx4aq.name == "7a-Buyer hands cash payment to Seller"
     assert len(activity_1qqx4aq.in_flows) == 1
@@ -130,7 +130,7 @@ def test_get_root():
     assert "Flow_0jmvvxc" in {flow.id for flow in activity_1qqx4aq.in_flows}
     assert "Flow_0znbe82" in {flow.id for flow in activity_1qqx4aq.out_flows}
 
-    activity_1rfm4sh = process.elements.get("Activity_1rfm4sh")
+    activity_1rfm4sh = process["Activity_1rfm4sh"]
     assert activity_1rfm4sh is not None
     assert activity_1rfm4sh.name == "7b-Seller hands Backpack to Buyer"
     assert len(activity_1rfm4sh.in_flows) == 1
@@ -139,20 +139,21 @@ def test_get_root():
     assert "Flow_1sx1rdt" in {flow.id for flow in activity_1rfm4sh.out_flows}
 
     # End event
-    end_event_0wqympo = process.elements.get("Event_0wqympo")
+    end_event_0wqympo = process["Event_0wqympo"]
     assert end_event_0wqympo is not None
     assert end_event_0wqympo.name == "Purchase Failed"
     assert len(end_event_0wqympo.in_flows) == 1
     assert "Flow_0diuub0" in {flow.id for flow in end_event_0wqympo.in_flows}
 
-    end_event_1y6wxsp = process.elements.get("Event_1y6wxsp")
+    end_event_1y6wxsp = process["Event_1y6wxsp"]
     assert end_event_1y6wxsp is not None
     assert end_event_1y6wxsp.name == "Purchase Completed"
     assert len(end_event_1y6wxsp.in_flows) == 1
     assert "Flow_1cm84v3" in {flow.id for flow in end_event_1y6wxsp.in_flows}
 
     # Start event
-    start_event_1y8wbre = process.elements.get("StartEvent_1y8wbre")
+    start_event_1y8wbre = process["StartEvent_1y8wbre"]
+    assert "StartEvent_1y8wbre" in process._start_states
     assert start_event_1y8wbre is not None
     assert start_event_1y8wbre.name == "Start7"
     assert len(start_event_1y8wbre.out_flows) == 1
@@ -162,3 +163,10 @@ def test_get_root():
     # Flows
     for flow_id, source_id, target_id in flows_to_test:
         assert_flow(process, flow_id, source_id, target_id)
+
+
+# def test_listener_and_parser():
+#     promela_gen_listener = PromelaGenListener()
+#     bpmn_traverser = BpmnTraverser(promela_gen_listener)
+
+#     bpmn_traverser.walk()
