b699f55377fb99ca5ab2e49156fe70aef067c2ad
Ty Klabacka <klabacka.ty@gmail.com>
2024-10-23 23:54:58 +0000

    moved xml ingest

--- src/bpmncwpverify/bpmn/BPMN.py
+++ src/bpmncwpverify/bpmn/BPMN.py
@@ -1,5 +1,13 @@
-from typing import Optional, List, Dict
+from typing import List, Dict
 from xml.etree.ElementTree import Element
+from defusedxml.ElementTree import parse
+
+################
+# Constants
+################
+from bpmncwpverify.constants import NAMESPACES
+
+################
 
 
 # Base class for all BPMN elements
@@ -79,9 +87,8 @@ class Flow(BpmnElement):
         element: Element,
     ) -> None:
         super().__init__(element)
-        self.source_node: Optional[Node] = None
-        self.target_node: Optional[Node] = None
-        self.seen = False
+        self.source_node: Node
+        self.target_node: Node
 
 
 class SequenceFlow(Flow):
@@ -100,9 +107,91 @@ class Process(BpmnElement):
         super().__init__(element)
         self.elements: Dict[str, Node] = {}
         self.flows: Dict[str, Flow] = {}
-        self.adj_list: Dict[str, List[str]] = {}
 
 
 class Bpmn:
+    _TAG_CLASS_MAPPING = {
+        "task": Node,
+        "startEvent": StartEvent,
+        "endEvent": EndEvent,
+        "exclusiveGateway": ExclusiveGatewayNode,
+        "parallelGateway": ParallelGatewayNode,
+    }
+
+    _FLOW_MAPPING = {"sequenceFlow": SequenceFlow}
+
     def __init__(self) -> None:
         self.processes: List[Process] = []
+
+    def __repr__(self) -> str:
+        build_arr: List[str] = []
+        for process in self.processes:
+            build_arr.append(f"Process ID: {process.id}, Name: {process.name}")
+            for element_id, element in process.elements.items():
+                build_arr.append(f"  Element ID: {element_id}, Name: {element.name}")
+                build_arr.append("    Outgoing to:")
+                for flow in element.out_flows:
+                    target_element = process.elements.get(flow.target_node.id)
+                    target_name = target_element.name if target_element else "Unknown"
+                    build_arr.append(
+                        f"      Element ID: {flow.target_node.id}, Name: {target_name}"
+                    )
+            build_arr.append("\n")
+
+        return "\n".join(build_arr)
+
+    def _build_graph(self, process: Process) -> None:
+        for element_id, element_instance in process.elements.items():
+            for outgoing in element_instance.element.findall(
+                "bpmn:outgoing", NAMESPACES
+            ):
+                flow_id = outgoing.text
+                if not flow_id:
+                    raise Exception("flow id is None")
+                flow = process.flows.get(flow_id.strip())
+                if flow is not None:
+                    source_ref = flow.element.attrib["sourceRef"]
+                    target_ref = flow.element.attrib["targetRef"]
+
+                    # update flow's source_node
+                    flow.source_node = process.elements[source_ref]
+                    # update flow's target_node
+                    flow.target_node = process.elements[target_ref]
+
+                    # update source node's out flows array
+                    process.elements[source_ref].out_flows.append(flow)
+                    # update target node's in flows array
+                    process.elements[target_ref].in_flows.append(flow)
+
+    def _traverse_process(self, process_element: Element) -> Process:
+        process = Process(process_element)
+
+        for element in process_element:
+            tag_local = element.tag.partition("}")[2]
+            if tag_local in Bpmn._TAG_CLASS_MAPPING:
+                element_class = Bpmn._TAG_CLASS_MAPPING[tag_local]
+                element_instance = element_class(element)
+                element_id = element_instance.id
+                process.elements[element_id] = element_instance
+
+            elif tag_local in Bpmn._FLOW_MAPPING:
+                flow_id = element.attrib["id"]
+                element_class = Bpmn._FLOW_MAPPING[tag_local]
+                element_instance = element_class(element)
+                process.flows[flow_id] = element_instance
+
+        self._build_graph(process)
+
+        return process
+
+    @staticmethod
+    def get_bpmn_from_xml(xml_file: str) -> "Bpmn":
+        tree = parse(xml_file)
+        root = tree.getroot()
+        bpmn = Bpmn()
+        processes = root.findall("bpmn:process", NAMESPACES)
+        for process_element in processes:
+            process = bpmn._traverse_process(process_element)
+            bpmn.processes.append(process)
+
+        return bpmn
