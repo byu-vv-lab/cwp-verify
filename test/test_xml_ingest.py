f014f169346dd784942fd2d1b7e86d145e7dfdac
Ty Klabacka <klabacka.ty@gmail.com>
2024-10-24 19:07:52 +0000

    added mocks for testing visitor

--- test/test_xml_ingest.py
+++ test/test_xml_ingest.py
@@ -1,7 +1,19 @@
 # type: ignore
+import unittest
+from xml.etree.ElementTree import Element
+from unittest.mock import MagicMock
 from returns.pipeline import is_successful
 from bpmncwpverify.promela_gen_visitor import PromelaGenVisitor
-from bpmncwpverify.BPMN import Bpmn, ParallelGatewayNode
+from bpmncwpverify.BPMN import (
+    Bpmn,
+    ParallelGatewayNode,
+    Task,
+    StartEvent,
+    EndEvent,
+    Process,
+    SequenceFlow,
+    ExclusiveGatewayNode,
+)
 import os
 
 # List of flows with their source and target node IDs
@@ -174,9 +186,60 @@ def test_xml_parser():
         assert_flow(process, flow_id, source_id, target_id, is_back_edge)
 
 
-def test_listener_and_parser():
-    bpmn = Bpmn.from_xml(workflow_bpmn_path)
-    visitor = PromelaGenVisitor()
-    assert is_successful(bpmn)
-    bpmn = bpmn.unwrap()
-    bpmn.accept(visitor)
+class TestBpmnFlowTraversal(unittest.TestCase):
+    def setUp(self):
+        # Mocking the visitor
+        self.visitor = MagicMock(spec=PromelaGenVisitor)
+        mock_element = MagicMock(spec=Element)
+        self.process = Process(mock_element)
+
+        self.process._elements = {
+            "Gateway_1pm4ghz": ExclusiveGatewayNode(mock_element),
+            "Gateway_12r266n": ParallelGatewayNode(mock_element),
+            "Activity_1qm7qck": Task(mock_element),
+            "Activity_1unsjkg": Task(mock_element),
+            "Activity_1t579ox": Task(mock_element),
+            "Gateway_0s1i42o": ParallelGatewayNode(mock_element),
+            "Activity_1bckz5y": Task(mock_element),
+            "Activity_1mktua2": Task(mock_element),
+            "Activity_0a5xzqf": Task(mock_element),
+            "Event_0wqympo": EndEvent(mock_element),
+            "Gateway_000lymc": ParallelGatewayNode(mock_element),
+            "Activity_1qqx4aq": Task(mock_element),
+            "Activity_1rfm4sh": Task(mock_element),
+            "Gateway_0cy7rs7": ParallelGatewayNode(mock_element),
+            "Event_1y6wxsp": EndEvent(mock_element),
+        }
+
+        self.process.flows = {}
+
+        self.process._start_states = {
+            "StartEvent_1y8wbre": StartEvent(mock_element),
+        }
+
+        self.process.all_items = self.process._elements | self.process._start_states
+
+        for node_id, node in self.process.all_items.items():
+            node.out_flows = []
+
+        for flow_id, source_id, target_id, is_back_edge in flows_to_test:
+            source_node = self.process.all_items[source_id]
+            target_node = self.process.all_items[target_id]
+
+            flow = SequenceFlow(mock_element)
+            flow.is_back_edge = is_back_edge
+            flow.target_node = target_node
+            self.process.flows[flow_id] = flow
+
+            source_node.out_flows.append(flow)
+            flow.source_node = source_node
+
+        self.visitor.visitSequenceFlow.return_value = True
+        self.visitor.visitTask.return_value = True
+        self.visitor.visitExclusiveGateway.return_value = True
+
+        self.bpmn = Bpmn()
+        self.bpmn.processes = [self.process]
+
+    def test_flow_traversal(self):
+        self.bpmn.accept(self.visitor)
