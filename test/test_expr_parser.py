6bdd0475c64ad657a812e0dd563f058a1392698d
Ty Klabacka <klabacka.ty@gmail.com>
2024-11-13 22:51:05 +0000

    Add more tests

--- test/test_expr_parser.py
+++ test/test_expr_parser.py
@@ -1,11 +1,15 @@
-# type: ignore
-
+from bpmncwpverify.expr import ExpressionListener
 import pytest
+from bpmncwpverify.antlr.ExprLexer import ExprLexer
+from bpmncwpverify.antlr.ExprParser import ExprParser
+
+from antlr4.error.ErrorListener import ErrorListener
+from returns.pipeline import is_successful
+
+from bpmncwpverify.state import SymbolTable
 
 from antlr4 import CommonTokenStream, InputStream
 
-from bpmncwpverify.antlr.ExprLexer import ExprLexer
-from bpmncwpverify.antlr.ExprParser import ExprParser
 
 from typing import Iterable
 
@@ -164,3 +168,89 @@ def test_or_input_test(or_input):
     parser = ExprParser(stream)
     id = parser.orExpr()
     assert id is not None
+
+
+class ThrowingErrorListener(ErrorListener):
+    def __init__(self):
+        super(ThrowingErrorListener, self).__init__()
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        raise SyntaxError(f"line {line}:{column} {msg}")
+
+
+def parse_input(input_text):
+    input_stream = InputStream(input_text)
+    lexer = ExprLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = ExprParser(stream)
+
+    parser.removeErrorListeners()
+    lexer.removeErrorListeners()
+
+    error_listener = ThrowingErrorListener()
+    parser.addErrorListener(error_listener)
+    lexer.addErrorListener(error_listener)
+
+    tree = parser.expr()
+    return tree
+
+
+@pytest.mark.parametrize(
+    "input_text",
+    [
+        "(x + y) != (z + t)",
+        "!((a - b) == (c * d))",
+        "(x > y) && (y < z)",
+        "(p >= q) || (r <= s)",
+        "a > b && b < c",
+        "a && b < c",
+        "!((x + (y * z)) == ((a / b) - c))",
+        "(((x)) + ((y))) > ((z))",
+        "((a + b) > c) && ((d - e) <= f) || !(g == h)",
+        "(x1 + y3d) == (y2 - yy_2)",
+        "(a * b + c / d - e) != (f + g)",
+    ],
+)
+def test_valid_inputs(input_text):
+    try:
+        parse_input(input_text)
+        assert True
+    except SyntaxError as e:
+        pytest.fail(f"Valid input raised SyntaxError: {e}")
+
+
+@pytest.mark.parametrize(
+    "input_text",
+    [
+        "(x y)",
+        "((a + b) > c",
+        "(a + b) > c$",
+        "(a ++ b) == c",
+        "(a + b c) == c",
+        "",
+        "(a +- 1.2.3) == b",
+        "( +- 1.2.3) == b",
+    ],
+)
+def test_invalid_inputs(input_text):
+    with pytest.raises(SyntaxError):
+        parse_input(input_text)
+
+
+@pytest.mark.parametrize(
+    "state, expression",
+    [
+        ("const a: bit = 0 var i : bit = 0 {0 1}", "a != i"),
+        ("const x: int = 0 var y : int = 0 {0 1} var z : int = 0", "(x > y) + (y < z)"),
+        # ("var a: short = 0 var b: short = 3 var c: int = 4 var d: int = 5 var e: int = 6 var f: int = 7 var g: int = 8 var h: int = 9, "((a + b) > c) && ((d - e) <= f) || !(g == h)"),
+    ],
+)
+def test_given_good_state_when_build_then_success(state, expression):
+    sym_table_result = SymbolTable.build(state)
+
+    assert is_successful(sym_table_result)
+    symbol_table: SymbolTable = sym_table_result.unwrap()
+
+    expr_checker_result = ExpressionListener.build(expression, symbol_table)
+
+    assert is_successful(expr_checker_result)
