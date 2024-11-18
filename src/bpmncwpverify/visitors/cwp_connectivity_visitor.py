6cc3a00f4c9c8803a12a8026c32996788ce28e7c
Ty Klabacka <klabacka.ty@gmail.com>
2024-11-18 01:47:27 +0000

    Add clarifying comments

--- src/bpmncwpverify/visitors/cwp_connectivity_visitor.py
+++ src/bpmncwpverify/visitors/cwp_connectivity_visitor.py
@@ -11,10 +11,12 @@ class CwpConnectivityVisitor(CwpVisitor):  # type: ignore
             self.visited.add(state)
 
     def visit_edge(self, edge: CwpEdge) -> None:
+        # This determines if an edge is a leaf node
         if edge.dest in self.visited:
             edge.is_leaf = True
 
     def end_visit_cwp(self, model: Cwp) -> None:
+        # This checks to see if any part of the graph is not connected
         if self.visited != set(model.states.values()) | set(
             model.start_states.values()
         ):
