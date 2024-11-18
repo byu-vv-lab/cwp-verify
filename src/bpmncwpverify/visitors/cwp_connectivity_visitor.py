57efe28404f973bc4d9a37ccea3a71a029d21f78
Ty Klabacka <klabacka.ty@gmail.com>
2024-11-18 01:44:36 +0000

    Add Graph Connectivity check to visitor

--- src/bpmncwpverify/visitors/cwp_connectivity_visitor.py
+++ src/bpmncwpverify/visitors/cwp_connectivity_visitor.py
@@ -1,5 +1,5 @@
 from typing import Set
-from bpmncwpverify.core.cwp import CwpState, CwpVisitor, CwpEdge
+from bpmncwpverify.core.cwp import Cwp, CwpState, CwpVisitor, CwpEdge
 
 
 class CwpConnectivityVisitor(CwpVisitor):  # type: ignore
@@ -10,12 +10,12 @@ class CwpConnectivityVisitor(CwpVisitor):  # type: ignore
         if state not in self.visited:
             self.visited.add(state)
 
-    def end_visit_state(self, state: CwpState) -> None:
-        pass
-
     def visit_edge(self, edge: CwpEdge) -> None:
         if edge.dest in self.visited:
             edge.is_leaf = True
 
-    def end_visit_edge(self, edge: CwpEdge) -> None:
-        pass
+    def end_visit_cwp(self, model: Cwp) -> None:
+        if self.visited != set(model.states.values()) | set(
+            model.start_states.values()
+        ):
+            raise Exception("Graph is not connected")
