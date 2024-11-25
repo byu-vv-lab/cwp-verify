0425bc1d86014c7de82c5634839cc92784a3b7b1
Ty Klabacka <klabacka.ty@gmail.com>
2024-11-25 18:56:30 +0000

    Add test for ensure_out_messages

--- src/bpmncwpverify/visitors/bpmn_connectivity_visitor.py
+++ src/bpmncwpverify/visitors/bpmn_connectivity_visitor.py
@@ -21,6 +21,13 @@ class BpmnConnectivityVisitor(BpmnVisitor):  # type: ignore
                     f"Exception occurred while visiting {obj_type}:{node.id}. A message flow can only go to a Message start or intermediate event; Receive, User, or Service task; Subprocess; or black box pool."
                 )
 
+    def _ensure_out_messages(self, node: Node, obj_type: str) -> None:
+        if node.out_msgs:
+            if not node.message_event_definition:
+                raise Exception(
+                    f"Exception occurred while visiting {obj_type}:{node.id}. A message flow can only come from a Messege end or intermediate event; Send, User, or Service task; Subprocess; or black box pool."
+                )
+
     def visit_start_event(self, event: StartEvent) -> bool:
         self._ensure_in_messages(event, "start event")
         return True
@@ -30,10 +37,12 @@ class BpmnConnectivityVisitor(BpmnVisitor):  # type: ignore
             raise Exception(
                 f"Exception occurred while visiting end event: {event.id}. End events cannot have incoming messages."
             )
+        self._ensure_out_messages(event, "end event")
         return True
 
     def visit_intermediate_event(self, event: IntermediateEvent) -> bool:
         self._ensure_in_messages(event, "intermediate event")
+        self._ensure_out_messages(event, "intermediate event")
         return True
 
     def visit_task(self, task: Task) -> bool:
--- test/visitors/test_bpmn_connectivity_visitor.py
+++ test/visitors/test_bpmn_connectivity_visitor.py
@@ -16,3 +16,42 @@ def test_ensure_in_messages_no_message_event_def(mocker):
         match="Exception occurred while visiting event:123. A message flow can only go to a Message start or intermediate event; Receive, User, or Service task; Subprocess; or black box pool.",
     ):
         visitor._ensure_in_messages(test_node, "event")
+
+
+def test_ensure_in_messages_with_message_event_def(mocker):
+    test_node = mocker.Mock(spec=Node)
+    test_node.id = "123"
+    test_node.in_msgs = [1]
+    test_node.message_event_definition = "test-id"
+
+    visitor = BpmnConnectivityVisitor()
+
+    # no error thrown
+    visitor._ensure_in_messages(test_node, "event")
+
+
+def test_ensure_out_messages_no_message_event_def(mocker):
+    test_node = mocker.Mock(spec=Node)
+    test_node.id = "123"
+    test_node.out_msgs = [1]
+    test_node.message_event_definition = ""
+
+    visitor = BpmnConnectivityVisitor()
+
+    with pytest.raises(
+        Exception,
+        match="Exception occurred while visiting event:123. A message flow can only come from a Messege end or intermediate event; Send, User, or Service task; Subprocess; or black box pool.",
+    ):
+        visitor._ensure_out_messages(test_node, "event")
+
+
+def test_ensure_out_messages_with_message_event_def(mocker):
+    test_node = mocker.Mock(spec=Node)
+    test_node.id = "123"
+    test_node.out_msgs = [1]
+    test_node.message_event_definition = "test-id"
+
+    visitor = BpmnConnectivityVisitor()
+
+    # no error thrown
+    visitor._ensure_out_messages(test_node, "event")
