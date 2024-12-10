c85871def70794f6a0784c75f9949f2f6d395615
Eric Mercer <egm@cs.byu.edu>
2024-12-10 22:08:29 +0000

    Refactor CLI to use builder and flows

--- run.py
+++
@@ -1,4 +0,0 @@
-from bpmncwpverify.cli import verify
-
-if __name__ == "__main__":
-    verify()
---
+++ test/test_cli.py
@@ -0,0 +1,133 @@
+# type: ignore
+import sys
+from returns.pipeline import is_successful
+from returns.functions import not_
+
+from bpmncwpverify.cli import _verify
+from bpmncwpverify.error import StateSyntaxError
+
+
+def test_givin_bad_state_file_path_when_verify_then_io_error(capsys):
+    # given
+    test_args = [
+        "verify",
+        "state.txt",
+        "./test/resources/test_cwp.xml",
+        "./test/resources/test_bpmn.bpmn",
+        "./test/resources/behavior.txt",
+    ]
+    sys.argv = test_args
+
+    # when
+    result = _verify()
+
+    # then
+    assert not_(is_successful)(result)
+    assert isinstance(result.failure(), FileNotFoundError)
+    error = result.failure()
+    assert isinstance(error, FileNotFoundError)
+    assert "state.txt" in str(error)
+
+
+def test_givin_bad_cwp_file_path_when_verify_then_io_error(capsys):
+    # given
+    test_args = [
+        "verify",
+        "./test/resources/state.txt",
+        "test_cwp.xml",
+        "./test/resources/test_bpmn.bpmn",
+        "./test/resources/behavior.txt",
+    ]
+    sys.argv = test_args
+
+    # when
+    result = _verify()
+
+    # then
+    assert not_(is_successful)(result)
+    error = result.failure()
+    assert isinstance(error, FileNotFoundError)
+    assert "test_cwp.xml" in str(error)
+
+
+def test_givin_bad_bpmn_file_path_when_verify_then_io_error(capsys):
+    # given
+    test_args = [
+        "verify",
+        "./test/resources/state.txt",
+        "./test/resources/test_cwp.xml",
+        "test_bpmn.bpmn",
+        "./test/resources/behavior.txt",
+    ]
+    sys.argv = test_args
+
+    # when
+    result = _verify()
+
+    # then
+    assert not_(is_successful)(result)
+    error = result.failure()
+    assert isinstance(error, FileNotFoundError)
+    assert "test_bpmn.bpmn" in str(error)
+
+
+def test_givin_bad_behavior_file_path_when_verify_then_io_error(capsys):
+    # given
+    test_args = [
+        "verify",
+        "./test/resources/state.txt",
+        "./test/resources/test_cwp.xml",
+        "./test/resources/test_bpmn.bpmn",
+        "behavior.txt",
+    ]
+    sys.argv = test_args
+
+    # when
+    result = _verify()
+
+    # then
+    assert not_(is_successful)(result)
+    error = result.failure()
+    assert isinstance(error, FileNotFoundError)
+    assert "behavior.txt" in str(error)
+
+
+def test_givin_bad_state_file_when_verify_then_state_errror(capsys):
+    # given
+    test_args = [
+        "verify",
+        "./test/resources/bad_state.txt",
+        "./test/resources/test_cwp.xml",
+        "./test/resources/test_bpmn.bpmn",
+        "./test/resources/behavior.txt",
+    ]
+    sys.argv = test_args
+
+    # when
+    result = _verify()
+
+    # then
+    assert not_(is_successful)(result)
+    error = result.failure()
+    assert isinstance(error, StateSyntaxError)
+
+
+def test_givin_good_files_when_verify_then_output_promela(capsys):
+    # given
+    test_args = [
+        "verify",
+        "./test/resources/state.txt",
+        "./test/resources/test_cwp.xml",
+        "./test/resources/test_bpmn.bpmn",
+        "./test/resources/behavior.txt",
+    ]
+    sys.argv = test_args
+
+    # when
+    result = _verify()
+
+    # then
+    assert is_successful(result)
+    outputs = result.unwrap()
+    assert outputs.promela is not None
+    assert outputs.promela != ""
