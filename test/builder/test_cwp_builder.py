6459efc15374373827be225771efb76a2c906547
Ty Klabacka <klabacka.ty@gmail.com>
2025-01-07 19:05:37 +0000

    Add tests for check expression

--- test/builder/test_cwp_builder.py
+++ test/builder/test_cwp_builder.py
@@ -1,15 +1,13 @@
 # type: ignore
 from bpmncwpverify.builder.cwp_builder import CwpBuilder
-from bpmncwpverify.core.cwp import Cwp, CwpEdge, CwpState
+from bpmncwpverify.core.cwp import Cwp
 from bpmncwpverify.core.error import (
     CwpEdgeNoParentExprError,
     CwpEdgeNoStateError,
-    CwpNoStartStateError,
+    CwpNoParentEdgeError,
 )
 from returns.result import Success
 import pytest
-from returns.pipeline import is_successful
-from returns.functions import not_
 
 from xml.etree.ElementTree import Element, SubElement
 
@@ -102,94 +100,29 @@ def test_from_xml_missing_parent_or_expression(mocker):
     assert isinstance(exc_info.value.args[0], CwpEdgeNoParentExprError)
 
 
-def test_parse_edges(mocker, builder):
-    mock_set_source = mocker.patch.object(CwpEdge, "set_source")
-    mock_set_dest = mocker.patch.object(CwpEdge, "set_dest")
-
-    source_state = mocker.MagicMock()
-    dest_state = mocker.MagicMock()
-    source_state.out_edges = []
-    dest_state.in_edges = []
-    builder._cwp.states = {
-        "sourceState": source_state,
-        "destState": dest_state,
-    }
-
-    mx_edge = mocker.MagicMock(spec=Element)
-    mx_edge.get.side_effect = lambda x: {
-        "source": "sourceState",
-        "target": "destState",
-        "id": "123",
-    }.get(x)
-    mx_edges = [mx_edge]
-
-    builder._parse_edges(mx_edges)
-
-    mock_set_source.assert_called_once_with(source_state)
-    mock_set_dest.assert_called_once_with(dest_state)
-
-
-def test_add_and_check_expressions(mocker, builder):
+def test_check_expression(mocker):
     mock_expr_checker = mocker.MagicMock()
-    mock_expr_checker.build.return_value = Success("bool")
-    edge = create_mock_edge(mocker, "edge1")
-    builder._cwp.edges = {"edge1": edge}
+    mock_expr_checker.type_check.return_value = Success(None)
 
-    all_items = [
-        Element(
-            "mxCell",
-            attrib={
-                "id": "expr1",
-                "parent": "edge1",
-                "value": "someExpr",
-                "style": "edgeLabel",
-            },
+    symbol_table = mocker.MagicMock()
+
+    builder = CwpBuilder()
+    builder._cwp = Cwp()
+    builder._cwp.edges = {"edge": mocker.MagicMock(), "parent": mocker.MagicMock()}
+
+    expression = "expression"
+
+    builder.check_expression(mock_expr_checker, expression, "parent", symbol_table)
+
+
+def test_check_expression_no_parent(mocker):
+    builder = CwpBuilder()
+    builder._cwp = Cwp()
+    builder._cwp.edges = {"edge": mocker.MagicMock()}
+
+    with pytest.raises(Exception) as exc_info:
+        builder.check_expression(
+            mocker.Mock(), mocker.Mock(), mocker.Mock(), mocker.Mock()
         )
-    ]
 
-    builder._add_and_check_expressions(all_items, mock_expr_checker)
-    mock_expr_checker.type_check.assert_called_once_with(
-        "someExpr", builder.symbol_table
-    )
-    assert edge.parent_id == "expr1"
-
-
-def test_build(mocker):
-    states: dict[str, CwpState] = {
-        "state1": mocker.MagicMock(spec=CwpState, in_edges=[], out_edges=["edge1"]),
-        "state2": mocker.MagicMock(
-            spec=CwpState, in_edges=["edge1"], out_edges=["edge2"]
-        ),
-        "state3": mocker.MagicMock(spec=CwpState, in_edges=["edge2"], out_edges=[]),
-    }
-    edges = {"edge1": mocker.MagicMock(), "edge2": mocker.MagicMock()}
-
-    mock_cwp = mocker.MagicMock()
-    mock_cwp.states = states
-    mock_cwp.edges = edges
-
-    obj = CwpBuilder()
-    obj._cwp = mock_cwp
-    obj.check_expressions = mocker.MagicMock()
-    mock_cwp.accept = mocker.MagicMock()
-
-    result = obj.build()
-
-    assert isinstance(result, Success)
-    assert result.unwrap() == mock_cwp
-
-    start_state = states["state1"]
-    end_states = [states["state3"]]
-    assert mock_cwp.start_state == start_state
-    assert list(end_states) == [states["state3"]]
-
-    mock_cwp.accept.assert_called_once()
-
-    new_edge = CwpEdge(mocker.MagicMock(), mocker.MagicMock())
-    states["state1"].in_edges = [new_edge]
-    states["state3"].out_edges = [new_edge]
-
-    result = obj.build()
-
-    assert not_(is_successful)(result)
-    assert isinstance(result.failure(), CwpNoStartStateError)
+    assert isinstance(exc_info.value.args[0], CwpNoParentEdgeError)
