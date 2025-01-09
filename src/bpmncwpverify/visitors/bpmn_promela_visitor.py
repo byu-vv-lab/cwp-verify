bb7edd927198cefd3be9c7d219fa667ccd06b5c0
Ty Klabacka <klabacka.ty@gmail.com>
2025-01-09 20:16:15 +0000

    Add indentation attribute and output promela

--- src/bpmncwpverify/visitors/bpmn_promela_visitor.py
+++ src/bpmncwpverify/visitors/bpmn_promela_visitor.py
@@ -14,11 +14,35 @@ from bpmncwpverify.core.bpmn import (
 ##############
 # Constants
 ##############
-HELPER_FUNCS = "#define hasToken(place) (place != 0)\n\n#define putToken(place) place = 1\n\n#define consumeToken(place) place = 0\n"
+HELPER_FUNCS_STR = "\n\n#define hasToken(place) (place != 0)\n\n#define putToken(place) place = 1\n\n#define consumeToken(place) place = 0"
 ##############
 
 
 class PromelaGenVisitor(BpmnVisitor):  # type: ignore
+    def __init__(self) -> None:
+        self.promela = ""
+        self.indent = 0
+
+    def _inc_indent(self) -> None:
+        self.indent += 1
+
+    def _dec_indent(self) -> None:
+        self.indent -= 1
+
+    def _tab(self) -> str:
+        """return string contianing 'self.indent' tabs"""
+        return "\t" * self.indent
+
+    def _nl(self, nl: int) -> str:
+        """return string contianing 'nl' new lines"""
+        return "\n" * nl
+
+    def _write_str(self, new_str: str, nl: int) -> None:
+        self.promela += "{}{}{}".format(self._tab(), new_str, self._nl(nl))
+
+    def __repr__(self) -> str:
+        return self.promela
+
     ####################
     # Visitor Methods
     ####################
@@ -47,13 +71,18 @@ class PromelaGenVisitor(BpmnVisitor):  # type: ignore
         return True
 
     def visit_process(self, process: Process) -> bool:
+        self._write_str(f"run {process.name}()", 1)
         return True
 
     def end_visit_process(self, process: Process) -> None:
         pass
 
     def visit_bpmn(self, bpmn: Bpmn) -> bool:
+        self._write_str(HELPER_FUNCS_STR, 2)
+        self._write_str("init {", 1)
+        self._inc_indent()
         return True
 
     def end_visit_bpmn(self, bpmn: Bpmn) -> None:
-        pass
+        self._dec_indent()
+        self._write_str("}", 1)
