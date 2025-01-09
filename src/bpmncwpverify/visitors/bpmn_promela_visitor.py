6f154d51be274fd9c9c3253798f574edeadd1515
Ty Klabacka <klabacka.ty@gmail.com>
2025-01-09 20:33:46 +0000

    Add new str attributes

--- src/bpmncwpverify/visitors/bpmn_promela_visitor.py
+++ src/bpmncwpverify/visitors/bpmn_promela_visitor.py
@@ -1,3 +1,4 @@
+from typing import List
 from bpmncwpverify.core.bpmn import (
     StartEvent,
     EndEvent,
@@ -20,8 +21,10 @@ HELPER_FUNCS_STR = "\n\n#define hasToken(place) (place != 0)\n\n#define putToken
 
 class PromelaGenVisitor(BpmnVisitor):  # type: ignore
     def __init__(self) -> None:
-        self.promela = ""
-        self.indent = 0
+        self.defs: List[str] = []
+        self.init_proc_contents: List[str] = []
+        self.promela: List[str] = []
+        self.indent: int = 0
 
     def _inc_indent(self) -> None:
         self.indent += 1
@@ -37,11 +40,17 @@ class PromelaGenVisitor(BpmnVisitor):  # type: ignore
         """return string contianing 'nl' new lines"""
         return "\n" * nl
 
-    def _write_str(self, new_str: str, nl: int) -> None:
-        self.promela += "{}{}{}".format(self._tab(), new_str, self._nl(nl))
+    def _write_str(self, new_str: str, nl: int, instance_str: List[str]) -> None:
+        instance_str.append(self._tab())
+        instance_str.append(new_str)
+        instance_str.append(self._nl(nl))
 
     def __repr__(self) -> str:
-        return self.promela
+        return (
+            "".join(self.defs)
+            + "".join(self.init_proc_contents)
+            + "".join(self.promela)
+        )
 
     ####################
     # Visitor Methods
@@ -71,18 +80,18 @@ class PromelaGenVisitor(BpmnVisitor):  # type: ignore
         return True
 
     def visit_process(self, process: Process) -> bool:
-        self._write_str(f"run {process.name}()", 1)
+        self._write_str(f"run {process.name}()", 1, self.init_proc_contents)
         return True
 
     def end_visit_process(self, process: Process) -> None:
         pass
 
     def visit_bpmn(self, bpmn: Bpmn) -> bool:
-        self._write_str(HELPER_FUNCS_STR, 2)
-        self._write_str("init {", 1)
+        self._write_str(HELPER_FUNCS_STR, 2, self.defs)
+        self._write_str("init {", 1, self.init_proc_contents)
         self._inc_indent()
         return True
 
     def end_visit_bpmn(self, bpmn: Bpmn) -> None:
         self._dec_indent()
-        self._write_str("}", 1)
+        self._write_str("}", 1, self.init_proc_contents)
