649e8a5491204895715f3e2549bcaa34f7c35246
Ty Klabacka <klabacka.ty@gmail.com>
2025-01-10 01:04:26 +0000

    Add enum for indent action

--- src/bpmncwpverify/visitors/bpmn_promela_visitor.py
+++ src/bpmncwpverify/visitors/bpmn_promela_visitor.py
@@ -1,4 +1,5 @@
 from typing import List
+from enum import Enum
 from bpmncwpverify.core.bpmn import (
     StartEvent,
     EndEvent,
@@ -16,7 +17,13 @@ from bpmncwpverify.core.bpmn import (
 # Constants
 ##############
 HELPER_FUNCS_STR = "\n\n#define hasToken(place) (place != 0)\n\n#define putToken(place) place = 1\n\n#define consumeToken(place) place = 0"
+
+
 ##############
+class MutIndent(Enum):
+    NIL = 0
+    INC = 1
+    DEC = 2
 
 
 class PromelaGenVisitor(BpmnVisitor):  # type: ignore
@@ -33,16 +40,21 @@ class PromelaGenVisitor(BpmnVisitor):  # type: ignore
             """return string contianing 'nl' new lines"""
             return "\n" * nl
 
-        def inc_indent(self) -> None:
+        def _inc_indent(self) -> None:
             self.indent += 1
 
-        def dec_indent(self) -> None:
+        def _dec_indent(self) -> None:
+            assert self.indent > 0
             self.indent -= 1
 
-        def write_str(self, new_str: str, nl: int) -> None:
+        def write_str(self, new_str: str, nl: int, mut_indent: MutIndent) -> None:
+            if mut_indent == MutIndent.DEC:
+                self._dec_indent()
             self.contents.append(self._tab())
             self.contents.append(new_str)
             self.contents.append(self._nl(nl))
+            if mut_indent == MutIndent.INC:
+                self._inc_indent()
 
         def __repr__(self) -> str:
             return "".join(self.contents)
@@ -53,8 +65,10 @@ class PromelaGenVisitor(BpmnVisitor):  # type: ignore
         self.promela = PromelaGenVisitor.StringManager()
         self.indent: int = 0
 
-    def _write_str(self, new_str: str, nl: int, sm: StringManager) -> None:
-        sm.write_str(new_str, nl)
+    def _write_str(
+        self, new_str: str, nl: int, sm: StringManager, mut_indent: MutIndent
+    ) -> None:
+        sm.write_str(new_str, nl, mut_indent)
 
     def __repr__(self) -> str:
         return str(self.defs) + str(self.init_proc_contents) + str(self.promela)
@@ -87,19 +101,19 @@ class PromelaGenVisitor(BpmnVisitor):  # type: ignore
         return True
 
     def visit_process(self, process: Process) -> bool:
-        self._write_str(f"run {process.name}()", 1, self.init_proc_contents)
-        self._write_str("hello", 1, self.promela)
+        self._write_str(
+            f"run {process.name}()", 1, self.init_proc_contents, MutIndent.NIL
+        )
+        self._write_str(f"proctype {process.name}() {{", 1, self.promela, MutIndent.INC)
         return True
 
     def end_visit_process(self, process: Process) -> None:
         pass
 
     def visit_bpmn(self, bpmn: Bpmn) -> bool:
-        self._write_str(HELPER_FUNCS_STR, 2, self.defs)
-        self._write_str("init {", 1, self.init_proc_contents)
-        self.init_proc_contents.inc_indent()
+        self._write_str(HELPER_FUNCS_STR, 2, self.defs, MutIndent.INC)
+        self._write_str("init {", 1, self.init_proc_contents, MutIndent.INC)
         return True
 
     def end_visit_bpmn(self, bpmn: Bpmn) -> None:
-        self.init_proc_contents.dec_indent()
-        self._write_str("}", 2, self.init_proc_contents)
+        self._write_str("}", 2, self.init_proc_contents, MutIndent.DEC)
