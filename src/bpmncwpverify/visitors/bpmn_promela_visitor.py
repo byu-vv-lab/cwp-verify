0746f6d883d82dcd0e7b7c64022eea4064981017
Ty Klabacka <klabacka.ty@gmail.com>
2025-01-13 17:27:35 +0000

    Add gen behavior model and call in visitor

--- src/bpmncwpverify/visitors/bpmn_promela_visitor.py
+++ src/bpmncwpverify/visitors/bpmn_promela_visitor.py
@@ -98,6 +98,7 @@ class PromelaGenVisitor(BpmnVisitor):  # type: ignore
 
     def __init__(self) -> None:
         self.defs = PromelaGenVisitor.StringManager()
+        self.behaviors = PromelaGenVisitor.StringManager()
         self.init_proc_contents = PromelaGenVisitor.StringManager()
         self.promela = PromelaGenVisitor.StringManager()
 
@@ -180,13 +181,21 @@ class PromelaGenVisitor(BpmnVisitor):  # type: ignore
 
         return atomic_block
 
+    def _gen_behavior_model(self, element: Node) -> None:
+        self.behaviors.write_str(
+            f"inline {element.name}_BehaviorModel(){{", NL_SINGLE, IndentAction.INC
+        )
+        self.behaviors.write_str("skip", NL_SINGLE)
+        self.behaviors.write_str("}", NL_DOUBLE, IndentAction.DEC)
+
     def __repr__(self) -> str:
-        return f"{self.defs}{self.init_proc_contents}{self.promela}"
+        return f"{self.defs}{self.behaviors}{self.init_proc_contents}{self.promela}"
 
     ####################
     # Visitor Methods
     ####################
     def visit_start_event(self, event: StartEvent) -> bool:
+        self._gen_behavior_model(event)
         self.promela.write_str(f"putToken({event.name})", NL_SINGLE, IndentAction.NIL)
         self.promela.write_str("do", NL_SINGLE, IndentAction.NIL)
 
@@ -196,21 +205,26 @@ class PromelaGenVisitor(BpmnVisitor):  # type: ignore
         return True
 
     def visit_end_event(self, event: EndEvent) -> bool:
+        self._gen_behavior_model(event)
         return True
 
     def visit_intermediate_event(self, event: IntermediateEvent) -> bool:
+        self._gen_behavior_model(event)
         return True
 
     def visit_task(self, task: Task) -> bool:
+        self._gen_behavior_model(task)
         return True
 
     def visit_exclusive_gateway(self, gateway: ExclusiveGatewayNode) -> bool:
+        self._gen_behavior_model(gateway)
         return True
 
     def end_visit_exclusive_gateway(self, gateway: ExclusiveGatewayNode) -> None:
         pass
 
     def visit_parallel_gateway(self, gateway: ParallelGatewayNode) -> bool:
+        self._gen_behavior_model(gateway)
         return True
 
     def visit_message_flow(self, flow: MessageFlow) -> bool:
